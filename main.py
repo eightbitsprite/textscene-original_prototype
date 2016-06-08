56a49f69889acb6429addce286d74ae2f5f40d33
k_bright <vincent.yh.huang@gmail.com>
2016-06-08 01:05:05 -0700

    v1.0 finished - correctly reads from the file and produces the appropriate output. Note that no error checking exists at the moment - maybe that's a job for the code that'll deal with this Json format?

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ action description and italics will be applied.
 #			|!(?P<keyword>[\w]+)) [\s]* 
 #		|[\s]*)
 #		\b(?P<line>.[^#]+|) 
-#		(\#(?P<time>[\d]+[\.]?[\d]*)|) [.]*
+#		(\#(?P<time>([\d]+[\.]?|[\.])[\d]*)|) [.]*
 regex = re.compile(r"""
 	(
 		^@(								#Specify speaker/emote OR keyword	
@@ -43,42 +43,56 @@ regex = re.compile(r"""
 		) [\s]*
 		| 								#Otherwise, ignore whitespace until...
 	[\s]*)									
-	(?P<line>.[^#]+|)  					#matches the main 'line' group
-	(\#(?P<time>[\d]+[\.]?[\d]*)|) [.]*	#if present, match specified 'time'
+	(?P<line>.[^#]+						#matches the main 'line' group
+		|
+	)  					
+	(\#
+		(?P<time>([\d]+[\.]?|[\.])[\d]*)	#if present, match specified 'time'
+		|
+	) [.]*
 	""", re.VERBOSE)
 
+class scene:
+	def __init__(self, sceneLines):
+		self.lines = []
+		for line in sceneLines:
+			self.lines.append(vars(line).copy())
+
 class sceneLine:
-    def __init__(self, fields):
-        self.speaker = "" if fields["speaker"] is None else fields["speaker"]
-        self.emote = "" if fields["emote"] is None else fields["emote"]
-        self.line = "" if fields["line"] is None else fields["line"]
-        self.keyword = "" if fields["keyword"] is None else fields["keyword"]
-        self.time = 1.0 if fields["time"] is None else float(fields["time"])
+    def __init__(self, speaker, emote, line, keyword, time):
+        self.speaker = "" if speaker is None else speaker
+        self.emote = "" if emote is None else emote
+        self.line = "" if line is None else line
+        self.keyword = "" if keyword is None else keyword
+        self.time = 1.0 if time is None else float(time)
 
-    def __str__(self):
-    	keyword = "" if self.keyword == "" else self.keyword + " "
-    	emote = "" if self.emote == "" else " (<i>" + self.emote + "</i>): "
-    	string = str(self.speaker) + emote + keyword + self.line
-    	return string
+    #def __repr__(self):
 
-    def debugString(self):
-    	string =  "[SPEAKER: "+ str(self.speaker) + " (" + self.emote + ") "
-    	string += str(self.line) + " TIME:" + str(self.time) + " "
-    	string += "KEYWORD: " + str(self.keyword) + "]\n"
-    	return string
+	def __str__(self):
+		keyword = "" if self.keyword == "" else self.keyword + " "
+		emote = "" if self.emote == "" else " (<i>" + self.emote + "</i>): "
+		string = str(self.speaker) + emote + keyword + self.line
+		return string
+
+	def debugString(self):
+		string =  "[SPEAKER: "+ str(self.speaker) + " (" + self.emote + ") "
+		string += str(self.line) + " TIME:" + str(self.time) + " "
+		string += "KEYWORD: " + str(self.keyword) + "]\n"
+		return string
 
 
 
 def read(infile):
-	input = open(infile, "r")
+	inputf = open(infile, "r")
 	lines = [] 
-	for line in input:
+	for line in inputf:
 		if line[0] == "$":
 			print "IGNORING: " + line
 			continue
 		pLine = line.rstrip()
 		if (pLine):
 			lines.append(pLine) 
+	inputf.close()
 	return lines
 
 def process(lines):
@@ -87,23 +101,40 @@ def process(lines):
 		match_res = regex.match(line)
 		if match_res:
 			fields = match_res.groupdict()
-			sLine = sceneLine(fields)
+			sLine = sceneLine(fields["speaker"],fields["emote"],
+				fields["line"],fields["keyword"],fields["time"])
 			objects.append(sLine)
 	return objects
 
+def write(scene, outfile, compact):
+	outputf = open(outfile, "w") 
+	lines = []
+	for line in scene:
+		lines.append(vars(line))
+	fScene = {'lines':lines}
+	if compact:
+		json.dump(fScene, outputf)
+	else:
+		json.dump(fScene, outputf, indent=4, separators=(',', ': '))
+	outputf.flush()
+	outputf.close()
+	return True
 
-def processing(infile, outfile):
+def processing(infile, outfile, compact):
 	lines = read(infile)
-	objects = process(lines)
-	for obj in objects:
-		print obj.debugString()
-	return False
+	scene = process(lines)
+	#for obj in objects:
+		#print obj
+		#print obj.debugString()
+	result = write(scene, outfile, compact)
+	return result
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print "INPUT ERROR; try python main.py [inputfile] [outputfile]"
+	if len(sys.argv) != 4:
+		print "INPUT ERROR; try python main.py -[c(ompact)/p(retty)] [inputfile] [outputfile]"
 	else:
-		infile = sys.argv[1]
-		outfile = sys.argv[2]
-		result = processing(infile, outfile)
+		compact = True if sys.argv[1] == "-c" else False
+		infile = sys.argv[2]
+		outfile = sys.argv[3]
+		result = processing(infile, outfile, compact)
 		print "PROCESSING SUCCESSFUL" if result else "PROCESSING UNSUCCESSFUL"
